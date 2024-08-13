2eb5566ec69abf3a9e7e5efecaa899f5d003d011
Kartatz <105828205+Kartatz@users.noreply.github.com>
2024-08-12 23:31:29 -0300

    Update to GCC 14.2.0

--- build.sh
+++ build.sh
@@ -2,22 +2,24 @@
 
 set -eu
 
+declare -r workdir="${PWD}"
+
 declare -r revision="$(git rev-parse --short HEAD)"
 
 declare -r gmp_tarball='/tmp/gmp.tar.xz'
-declare -r gmp_directory='/tmp/gmp-6.2.1'
+declare -r gmp_directory='/tmp/gmp-6.3.0'
 
 declare -r mpfr_tarball='/tmp/mpfr.tar.xz'
-declare -r mpfr_directory='/tmp/mpfr-4.2.0'
+declare -r mpfr_directory='/tmp/mpfr-4.2.1'
 
 declare -r mpc_tarball='/tmp/mpc.tar.gz'
 declare -r mpc_directory='/tmp/mpc-1.3.1'
 
 declare -r binutils_tarball='/tmp/binutils.tar.xz'
-declare -r binutils_directory='/tmp/binutils-2.40'
+declare -r binutils_directory='/tmp/binutils-2.43'
 
-declare -r gcc_tarball='/tmp/gcc.tar.gz'
-declare -r gcc_directory='/tmp/gcc-13.2.0'
+declare -r gcc_tarball='/tmp/gcc.tar.xz'
+declare -r gcc_directory='/tmp/gcc-14.2.0'
 
 declare -r system_image='/tmp/dragonflybsd.iso'
 declare -r system_image_compressed='/tmp/dragonflybsd.iso.bz2'
@@ -28,7 +30,7 @@ declare -r triplet='x86_64-unknown-dragonfly'
 declare -r optflags='-Os'
 declare -r linkflags='-Wl,-s'
 
-declare -r max_jobs="$(($(nproc) * 8))"
+declare -r max_jobs="$(($(nproc) * 17))"
 
 declare build_type="${1}"
 
@@ -52,24 +54,38 @@ if ! (( is_native )); then
 	cross_compile_flags+="--host=${CROSS_COMPILE_TRIPLET}"
 fi
 
+if ! [ -f "${gmp_tarball}" ]; then
+	wget --no-verbose 'https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz' --output-document="${gmp_tarball}"
+	tar --directory="$(dirname "${gmp_directory}")" --extract --file="${gmp_tarball}"
+fi
+
+if ! [ -f "${mpfr_tarball}" ]; then
+	wget --no-verbose 'https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.1.tar.xz' --output-document="${mpfr_tarball}"
+	tar --directory="$(dirname "${mpfr_directory}")" --extract --file="${mpfr_tarball}"
+fi
+
+if ! [ -f "${mpc_tarball}" ]; then
+	wget --no-verbose 'https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz' --output-document="${mpc_tarball}"
+	tar --directory="$(dirname "${mpc_directory}")" --extract --file="${mpc_tarball}"
+fi
+
+if ! [ -f "${binutils_tarball}" ]; then
+	wget --no-verbose 'https://ftp.gnu.org/gnu/binutils/binutils-2.43.tar.xz' --output-document="${binutils_tarball}"
+	tar --directory="$(dirname "${binutils_directory}")" --extract --file="${binutils_tarball}"
+	
+	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/patches/0001-Revert-gold-Use-char16_t-char32_t-instead-of-uint16_.patch"
+fi
+
+if ! [ -f "${gcc_tarball}" ]; then
+	wget --no-verbose 'https://ftp.gnu.org/gnu/gcc/gcc-14.2.0/gcc-14.2.0.tar.xz' --output-document="${gcc_tarball}"
+	tar --directory="$(dirname "${gcc_directory}")" --extract --file="${gcc_tarball}"
+	
+	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-Revert-GCC-change-about-turning-Wimplicit-function-d.patch"
+fi
+
 declare -r toolchain_directory="/tmp/venti"
 
-wget --no-verbose 'https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.xz' --output-document="${gmp_tarball}"
-tar --directory="$(dirname "${gmp_directory}")" --extract --file="${gmp_tarball}"
-
-wget --no-verbose 'https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.0.tar.xz' --output-document="${mpfr_tarball}"
-tar --directory="$(dirname "${mpfr_directory}")" --extract --file="${mpfr_tarball}"
-
-wget --no-verbose 'https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz' --output-document="${mpc_tarball}"
-tar --directory="$(dirname "${mpc_directory}")" --extract --file="${mpc_tarball}"
-
-wget --no-verbose 'https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.xz' --output-document="${binutils_tarball}"
-tar --directory="$(dirname "${binutils_directory}")" --extract --file="${binutils_tarball}"
-
-wget --no-verbose 'https://mirrors.kernel.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.xz' --output-document="${gcc_tarball}"
-tar --directory="$(dirname "${gcc_directory}")" --extract --file="${gcc_tarball}"
-
-wget --no-verbose 'https://mirror-master.dragonflybsd.org/iso-images/dfly-x86_64-5.0.0_REL.iso.bz2'  --output-document="${system_image_compressed}"
+wget --no-verbose 'https://mirror-master.dragonflybsd.org/iso-images/dfly-x86_64-5.0.0_REL.iso.bz2' --output-document="${system_image_compressed}"
 
 pushd "$(dirname "${system_image_compressed}")"
 
@@ -145,8 +161,6 @@ cd "${mpc_directory}/build"
 make all --jobs
 make install
 
-sed -i 's/#include <stdint.h>/#include <stdint.h>\n#include <stdio.h>/g' "${toolchain_directory}/include/mpc.h"
-
 [ -d "${binutils_directory}/build" ] || mkdir "${binutils_directory}/build"
 
 cd "${binutils_directory}/build"
@@ -182,7 +196,7 @@ rm --force --recursive ./*
 	--with-mpfr="${toolchain_directory}" \
 	--with-bugurl='https://github.com/AmanoTeam/Venti/issues' \
 	--with-gcc-major-version-only \
-	--with-pkgversion="Venti v0.4-${revision}" \
+	--with-pkgversion="Venti v0.5-${revision}" \
 	--with-sysroot="${toolchain_directory}/${triplet}" \
 	--with-native-system-header-dir='/include' \
 	--includedir="${toolchain_directory}/${triplet}/include" \
