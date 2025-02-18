efaa20a407ecdc41b060ffe51e40b1e44d683bee
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-02-18 17:06:53 -0300

    Update to GCC 15.0.1

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 set -eu
 
 declare -r workdir="${PWD}"
+declare -r toolchain_directory='/tmp/venti'
+declare -r share_directory="${toolchain_directory}/usr/local/share/venti"
+
+declare -r environment="LD_LIBRARY_PATH=${toolchain_directory}/lib PATH=${PATH}:${toolchain_directory}/bin"
 
 declare -r revision="$(git rev-parse --short HEAD)"
 
@@ -16,18 +20,15 @@ declare -r mpc_tarball='/tmp/mpc.tar.gz'
 declare -r mpc_directory='/tmp/mpc-1.3.1'
 
 declare -r binutils_tarball='/tmp/binutils.tar.xz'
-declare -r binutils_directory='/tmp/binutils-2.43'
+declare -r binutils_directory='/tmp/binutils-with-gold-2.44'
 
 declare -r gcc_tarball='/tmp/gcc.tar.xz'
-declare -r gcc_directory='/tmp/gcc-14.2.0'
-
-declare -r system_image='/tmp/dragonflybsd.iso'
-declare -r system_image_compressed='/tmp/dragonflybsd.iso.bz2'
-declare -r system_directory='/tmp/dragonflybsd'
+declare -r gcc_directory='/tmp/gcc-master'
 
 declare -r triplet='x86_64-unknown-dragonfly'
+declare -r sysroot_url="https://github.com/AmanoTeam/dragonfly-sysroot/releases/latest/download/${triplet}.tar.xz"
 
-declare -r optflags='-Os'
+declare -r optflags='-w -Os'
 declare -r linkflags='-Wl,-s'
 
 declare -r max_jobs="$(($(nproc) * 17))"
@@ -44,13 +45,9 @@ if [ "${build_type}" == 'native' ]; then
 	is_native='1'
 fi
 
-declare OBGGCC_TOOLCHAIN='/tmp/obggcc-toolchain'
-declare CROSS_COMPILE_TRIPLET=''
-
 declare cross_compile_flags=''
 
 if ! (( is_native )); then
-	source "./submodules/obggcc/toolchains/${build_type}.sh"
 	cross_compile_flags+="--host=${CROSS_COMPILE_TRIPLET}"
 fi
 
@@ -70,46 +67,23 @@ if ! [ -f "${mpc_tarball}" ]; then
 fi
 
 if ! [ -f "${binutils_tarball}" ]; then
-	wget --no-verbose 'https://ftp.gnu.org/gnu/binutils/binutils-2.43.tar.xz' --output-document="${binutils_tarball}"
+	wget --no-verbose 'https://ftp.gnu.org/gnu/binutils/binutils-with-gold-2.44.tar.xz' --output-document="${binutils_tarball}"
 	tar --directory="$(dirname "${binutils_directory}")" --extract --file="${binutils_tarball}"
 	
-	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/patches/0001-Revert-gold-Use-char16_t-char32_t-instead-of-uint16_.patch"
+	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Revert-gold-Use-char16_t-char32_t-instead-of-uint16_.patch"
+	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Disable-annoying-linker-warnings.patch"
 fi
 
 if ! [ -f "${gcc_tarball}" ]; then
-	wget --no-verbose 'https://ftp.gnu.org/gnu/gcc/gcc-14.2.0/gcc-14.2.0.tar.xz' --output-document="${gcc_tarball}"
+	wget --no-verbose 'https://github.com/gcc-mirror/gcc/archive/refs/heads/master.tar.gz' --output-document="${gcc_tarball}"
 	tar --directory="$(dirname "${gcc_directory}")" --extract --file="${gcc_tarball}"
 	
-	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/0001-Revert-GCC-change-about-turning-Wimplicit-function-d.patch"
+	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Fix-libgcc-build-on-arm.patch"
+	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Change-the-default-language-version-for-C-compilatio.patch"
+	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Turn-Wimplicit-int-back-into-an-warning.patch"
+	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Turn-Wint-conversion-back-into-an-warning.patch"
+	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Revert-GCC-change-about-turning-Wimplicit-function-d.patch"
 fi
-
-declare -r toolchain_directory="/tmp/venti"
-
-wget --no-verbose 'https://mirror-master.dragonflybsd.org/iso-images/dfly-x86_64-5.0.0_REL.iso.bz2' --output-document="${system_image_compressed}"
-
-pushd "$(dirname "${system_image_compressed}")"
-
-bzip2 --decompress "${system_image_compressed}"
-
-pushd
-
-[ -d "${system_directory}" ] || mkdir "${system_directory}"
-
-sudo mount -o loop "${system_image}" "${system_directory}"
-
-[ -d "${toolchain_directory}/${triplet}" ] || mkdir --parent "${toolchain_directory}/${triplet}"
-
-cp --recursive "${system_directory}/lib" "${toolchain_directory}/${triplet}"
-cp --recursive "${system_directory}/usr/lib" "${toolchain_directory}/${triplet}"
-cp --recursive "${system_directory}/usr/include" "${toolchain_directory}/${triplet}"
-
-sudo umount "${system_directory}"
-
-pushd "${toolchain_directory}/${triplet}/lib"
-
-find . -type l | xargs ls -l | grep '/lib/' | awk '{print "unlink "$9" && ln -s $(basename "$11") $(basename "$9")"}'  | bash
-
-pushd
 
 [ -d "${gmp_directory}/build" ] || mkdir "${gmp_directory}/build"
 
@@ -183,6 +157,29 @@ rm --force --recursive ./*
 make all --jobs="${max_jobs}"
 make install
 
+cd "$(mktemp --directory)"
+
+declare sysroot_file="${PWD}/${triplet}.tar.xz"
+declare sysroot_directory="${PWD}/${triplet}"
+
+curl \
+	--url "${sysroot_url}" \
+	--retry '30' \
+	--retry-all-errors \
+	--retry-delay '0' \
+	--retry-max-time '0' \
+	--location \
+	--silent \
+	--output "${sysroot_file}"
+
+tar \
+	--extract \
+	--file="${sysroot_file}"
+
+cp --recursive "${sysroot_directory}" "${toolchain_directory}"
+
+rm --force --recursive "${PWD}"
+
 [ -d "${gcc_directory}/build" ] || mkdir "${gcc_directory}/build"
 
 cd "${gcc_directory}/build"
@@ -196,7 +193,7 @@ rm --force --recursive ./*
 	--with-mpfr="${toolchain_directory}" \
 	--with-bugurl='https://github.com/AmanoTeam/Venti/issues' \
 	--with-gcc-major-version-only \
-	--with-pkgversion="Venti v0.5-${revision}" \
+	--with-pkgversion="Venti v0.6-${revision}" \
 	--with-sysroot="${toolchain_directory}/${triplet}" \
 	--with-native-system-header-dir='/include' \
 	--includedir="${toolchain_directory}/${triplet}/include" \
@@ -217,6 +214,9 @@ rm --force --recursive ./*
 	--enable-languages='c,c++' \
 	--enable-ld \
 	--enable-gold \
+	--disable-plugin \
+	--disable-libsanitizer \
+	--disable-fixincludes \
 	--disable-libstdcxx-pch \
 	--disable-werror \
 	--disable-libgomp \
@@ -227,9 +227,15 @@ rm --force --recursive ./*
 	${cross_compile_flags} \
 	CFLAGS="${optflags}" \
 	CXXFLAGS="${optflags}" \
-	LDFLAGS="-Wl,-rpath-link,${OBGGCC_TOOLCHAIN}/${CROSS_COMPILE_TRIPLET}/lib ${linkflags}"
+	LDFLAGS="${linkflags}"
 
-LD_LIBRARY_PATH="${toolchain_directory}/lib" PATH="${PATH}:${toolchain_directory}/bin" make \
+declare args=''
+
+if (( is_native )); then
+	args+="${environment}"
+fi
+
+env ${args} make \
 	CFLAGS_FOR_TARGET="${optflags} ${linkflags}" \
 	CXXFLAGS_FOR_TARGET="${optflags} ${linkflags}" \
 	all --jobs="${max_jobs}"
@@ -237,14 +243,10 @@ make install
 
 cd "${toolchain_directory}/${triplet}/bin"
 
-for name in *; do
-	rm "${name}"
-	ln -s "../../bin/${triplet}-${name}" "${name}"
-done
-
-rm --recursive "${toolchain_directory}/share"
-rm --recursive "${toolchain_directory}/lib/gcc/${triplet}/"*"/include-fixed"
-
 patchelf --add-rpath '$ORIGIN/../../../../lib' "${toolchain_directory}/libexec/gcc/${triplet}/"*"/cc1"
 patchelf --add-rpath '$ORIGIN/../../../../lib' "${toolchain_directory}/libexec/gcc/${triplet}/"*"/cc1plus"
 patchelf --add-rpath '$ORIGIN/../../../../lib' "${toolchain_directory}/libexec/gcc/${triplet}/"*"/lto1"
+
+mkdir --parent "${share_directory}"
+
+cp --recursive "${workdir}/tools/dev/"* "${share_directory}"
